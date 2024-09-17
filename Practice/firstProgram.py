07c6ec58de3e61f970fe426ca62df7ebc749b7b6
Akib Hasan Niloy <akibhasanniloy2@gmail.com>
2024-09-18 00:51:49 +0600

    first program

---
+++ Practice/firstProgram.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+print("My name is Akib Hasan")
+print("My age is 22")
+
+print("I live is Bangladesh, and I am from Dhaka")
+print(22)
+
+name ="Akib"
+age =22
+price = 30.05
+print("My name is:", name)
+print("My age is:", age)
+
+print(type(name))
+print(type(age))
+print(type(price))
